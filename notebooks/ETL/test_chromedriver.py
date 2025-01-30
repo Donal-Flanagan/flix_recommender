ba86f692f8b55322579668040f31d6fb0de802ec
Donal Flanagan <donaloflanagain@gmail.com>
2025-01-30 05:57:57 +0100

    Removed script for checking selenium chromedriver. It turned out I just needed to turn off the computer and turn it back on again #ITCrowd. The webscraper is working now.

--- notebooks/ETL/test_chromedriver.py
+++
@@ -1,9 +0,0 @@
-# test_chromedriver.py
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-try:
-    print("Attempting to install WebDriver...")
-    driver_path = ChromeDriverManager().install()
-    print(f"WebDriver installed at: {driver_path}")
-except Exception as e:
-    print(f"Error installing WebDriver: {e}")
